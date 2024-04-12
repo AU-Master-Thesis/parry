ec83ead68622825ead5e7eedb2a7032ceac4bd00
Jens <jens.jens@live.dk>
2024-04-12 13:42:32 +0200

    feat(bevy): converting bevy 2d polygon to 2d polygon

--- src/transformation/to_bevy/bevy3d_to_parry2d.rs
+++ src/transformation/to_bevy/bevy3d_to_parry2d.rs
@@ -1,11 +1,12 @@
 use crate::{
-    math::{Real, Vector},
+    math::{Point, Real, Vector},
     shape,
 };
 #[cfg(feature = "bevy")]
 use bevy::math::primitives;
 use bevy::math::{Vec2, Vec3};
 
+// CUBOID
 // implement conversions for bevy::math::primitives::Cuboid and parry2d shape::Cuboid
 #[cfg(feature = "dim2")]
 impl Into<shape::Cuboid> for primitives::Cuboid {
@@ -42,6 +43,7 @@ impl Into<primitives::Cuboid> for shape::Cuboid {
     }
 }
 
+// SPHERE
 // implement conversions for bevy::math::primitives::Sphere and parry2d shape::Ball
 #[cfg(feature = "dim2")]
 impl Into<shape::Ball> for primitives::Sphere {
@@ -57,3 +59,57 @@ impl Into<shape::Ball> for primitives::Sphere {
         shape::Ball::new(self.radius as Real)
     }
 }
+
+// CYLINDER
+// implement conversions for bevy::math::primitives::Cylinder and parry2d shape::Ball
+#[cfg(feature = "dim2")]
+impl Into<shape::Ball> for primitives::Cylinder {
+    fn into(self) -> shape::Ball {
+        shape::Ball::new(self.radius as Real)
+    }
+}
+
+// implement conversions for bevy::math::primitives::Cylinder and parry3d shape::Cylinder
+#[cfg(feature = "dim3")]
+impl Into<shape::Cylinder> for primitives::Cylinder {
+    fn into(self) -> shape::Cylinder {
+        shape::Cylinder::new(self.half_height as Real, self.radius as Real)
+    }
+}
+
+// Triangle
+// implement conversions for bevy::math::primitives::Triangle and parry2d shape::Triangle
+#[cfg(feature = "dim2")]
+impl Into<shape::Triangle> for primitives::Polygon<3> {
+    fn into(self) -> shape::Triangle {
+        shape::Triangle::new(
+            Point::from([self.vertices[0].x as Real, self.vertices[0].y as Real]),
+            Point::from([self.vertices[1].x as Real, self.vertices[1].y as Real]),
+            Point::from([self.vertices[2].x as Real, self.vertices[2].y as Real]),
+        )
+    }
+}
+
+// // implement conversions for bevy::math::primitives::Triangle and parry3d shape::Triangle
+// #[cfg(feature = "dim3")]
+// impl Into<shape::Triangle> for primitives::Polygon<3> {
+//     fn into(self) -> shape::Triangle {
+//         shape::Triangle::new(
+//             Point::from([
+//                 self.vertices[0].x as Real,
+//                 self.vertices[0].y as Real,
+//                 self.vertices[0].z as Real,
+//             ]),
+//             Point::from([
+//                 self.vertices[1].x as Real,
+//                 self.vertices[1].y as Real,
+//                 self.vertices[1].z as Real,
+//             ]),
+//             Point::from([
+//                 self.vertices[2].x as Real,
+//                 self.vertices[2].y as Real,
+//                 self.vertices[2].z as Real,
+//             ]),
+//         )
+//     }
+// }
